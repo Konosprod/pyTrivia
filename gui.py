590fa91f1f5efa18b25b96491c5c9fec5ebb61b4
Kono <konosprod@free.fr>
2014-11-28 10:04:07 +0100

    Textbox filling empty space

--- gui.py
+++ gui.py
@@ -20,14 +20,14 @@ class Application():
         
     def createWidgets(self):
         self.t1 = tk.Label(self.mainWindow, text="Le saviez-vous ?")
-        self.t1.pack()
+        self.t1.pack(side="top", fill="x", expand=False)
         self.txtEntry1 = tk.Text(self.mainWindow, width=80, height=12, wrap="word")
-        self.txtEntry1.pack()
+        self.txtEntry1.pack(fill="both", expand=True)
         
         self.t2 = tk.Label(self.mainWindow, text="Lumière sur :")
-        self.t2.pack()
+        self.t2.pack(side="top", fill="x", expand=False)
         self.txtEntry2 = tk.Text(self.mainWindow, width=80, height=12, wrap="word")
-        self.txtEntry2.pack()
+        self.txtEntry2.pack(fill="both", expand=True)
         
         self.setText()
     
